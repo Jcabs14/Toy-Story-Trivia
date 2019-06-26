0943fef553874f186e5f266d2c95bc090681f6bb
Jared Cabanilla <Jcabanilla@gmail.com>
2019-06-25 17:32:53 -0700

    added images to image folder

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
     var correctCounter = 0;
     var incorrectCounter = 0;
     var questionCounter = 0;
-    var unanswered = 0;
+    var unansweredCounter = 0;
     var clockCounter = 30;
+    var userAnswer;
 
     //create an array of questions
     var questionArray = ["What year did Toy Story come out?", "Who is the actor who plays the voice of Woody?",
@@ -14,21 +15,45 @@ $(document).ready(function () {
         "What is the name of the owner of all the toys?"];
 
     //create an array of answers
-    var answerArray = [["1989", "1995", "2000", "2003"], ["Nicolas Cage","Tim Allen","Tom Hanks","Forest Whittiker"], 
-    ["Cid","Alien","Emporer Zurg","Woody"], ["Toy Story","Toy Story 2","Toy Story 3","Toy Story 4"], 
-    ["Woody's camp","Woody's house of fun","Woody's cool show","Woody's round up"], ["Pizza Planet","Pizza World","Pizza Universe","Buzz LightYears Pizza"], ["James","Brian","Andy","Mac"]];
+    var answerArray = [["1989", "1995", "2000", "2003"], ["Nicolas Cage", "Tim Allen", "Tom Hanks", "Forest Whittiker"],
+    ["Cid", "Alien", "Emporer Zurg", "Woody"], ["Toy Story", "Toy Story 2", "Toy Story 3", "Toy Story 4"],
+    ["Woody's camp", "Woody's house of fun", "Woody's cool show", "Woody's round up"], ["Pizza Planet", "Pizza World", "Pizza Universe", "Buzz LightYears Pizza"], 
+    ["James", "Brian", "Andy", "Mac"]];
 
     //create an array of images
-    var imageArray = [];
+    var imageArray = ["assets/images/correct1.gif","assets/images/correct2.gif","assets/images/correct3.gif","assets/images/correct4.gif",
+    "assets/images/correct5.gif","assets/images/correct6.gif","assets/images/correct7.gif",];
+
+    //create an array of correct answers"
+    var correctAnswerArray = ["B. 1995", "B. Tim Allen"]
 
     //create questions
     function createQuestions() {
         $("#questionArea").html("<p class='text-center timer-p'>Time Remaining: <span class='timer'>30</span></p><p class='text-center'>" + questionArray[questionCounter] + "</p><p class='first-answer answer'>A. " + answerArray[questionCounter][0] + "</p><p class='answer'>B. " + answerArray[questionCounter][1] + "</p><p class='answer'>C. " + answerArray[questionCounter][2] + "</p><p class='answer'>D. " + answerArray[questionCounter][3] + "</p>");
     }
 
-    //function when clock hits 0
-    function timeOut(){
 
+
+    //function when clock hits 0
+    function timeOut() {
+        unansweredCounter++;
+        $("#questionArea").html()
+        setTimeout(wait,4000);
+
+    }
+
+    //function to count correct answers
+    function correctAnswers() {
+        correctCounter++;
+        $("#questionArea").html();
+        setTimeout(wait, 4000);
+
+    }
+    //function to count wrong answers
+    function wrongAnswers() {
+        wrongCounter++;
+        $("#questionArea").html()
+        setTimeout(wait, 4000);
     }
 
     //create timer interval
@@ -45,22 +70,19 @@ $(document).ready(function () {
             $(".timer").html(clockCounter);
         }
     }
+    //function to go through questions
+    function wait() {
+        //ternary operator replacing if/else for generate more questions
+        if (questionCounter < 7) {
+            questionCounter++ ,
+                createQuestions(),
+                clockCounter = 30,
+                questionTimer();
+        }
+        else {
 
-
-
-    //function to count correct answers
-    function correctAnswers(){
-
-    }
-    //function to count wrong answers
-    function wrongAnswers(){
-
-    }
-        //on click function to move to the next question
-
-
-    //
-
+        }
+    };
     //start page to start the trivia
     //create starting page
     function startPage() {
@@ -71,6 +93,21 @@ $(document).ready(function () {
             createQuestions();
         });
         questionTimer();
+
+        //on click function to move to the next question
+
+        $(".answer").on("click", function (event) {
+            if (userAnswer === correctAnswerArray[questionCounter]) {
+
+
+            }
+            else {
+                timeOut();
+                wrongAnswers();
+
+            }
+        });
+
     };
 
     startPage();
